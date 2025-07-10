484d86af8b986f3cb79f95db7aaca25cd08ab88d
bhavani-mudrakola <bhavani.mudrakola@gmail.com>
2025-07-10 15:23:09 +0530

    first commit in main

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('myForm.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Get data from the form
+    username = request.form['username']
+    return render_template('greeting.html', name=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
