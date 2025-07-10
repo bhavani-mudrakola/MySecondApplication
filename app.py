630e03ecbb4c983f038712d7a9ec73428d28aa0e
bhavani-mudrakola <bhavani.mudrakola@gmail.com>
2025-07-10 15:36:33 +0530

    second commit on feature-1 branch

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('myForm.html')
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET','POST'])
 def submit():
     # Get data from the form
     username = request.form['username']
